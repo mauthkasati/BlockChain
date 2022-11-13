505d0de8f5655086e7b67f77bb4ba9c20ff7a2e1
mauthkasati <76538592+mauthkasati@users.noreply.github.com>
2022-11-13 21:58:56 +0200

    Add files via upload

---
+++ BlockChain_POW_java/Network.java
@@ -0,0 +1,167 @@
+
+import java.util.Random;
+import java.util.ArrayList;
+
+/**
+ * This class stores the entire blockchain network.
+ * This includes multiple nodes which mine and verify blocks
+ */
+public class Network {
+
+    private static int difficulty = 1; // default difficulty unless otherwise specified
+    private static int transactionNumber = 0;
+    private ArrayList<Miner> miners = new ArrayList<Miner>();
+    private ArrayList<Node> nodes = new ArrayList<Node>();
+    private BlockChain networkChain = new BlockChain();
+    private int minerNum = -1; // stores the integer value of the miner that mined the last block
+
+    /**
+     * Default constructor.
+     * Adds 1 miner and 1 node by default
+     */
+    public Network() {
+        this.addMiner();
+        this.addNode();
+    }
+
+    /**
+     * Modifier used to change mining difficulty across the network.
+     * 
+     * @param difficultyValue the number of leading zeros a valid hash must have
+     */
+    public static void setDifficulty(int difficultyValue) {
+        Network.difficulty = difficultyValue;
+    }
+
+    /**
+     * Accessor used to view the mining difficulty of the network.
+     * 
+     * @return the current mining difficult of the blockchain network
+     */
+    public static int getDifficulty() {
+        return Network.difficulty;
+    }
+
+    /**
+     * Creates a new miner object and adds it to the network's list of miners
+     */
+    public void addMiner() {
+        Miner newMiner = new Miner();
+        this.miners.add(newMiner);
+    }
+
+    /**
+     * Creates a new node object and adds it to the network's list of nodes
+     */
+    public void addNode() {
+        Node newNode = new Node();
+        this.nodes.add(newNode);
+    }
+
+    private Miner successfulMiner() {
+        /*
+         * This method picks a random miner from the list of miners to mine the newest
+         * block
+         * This is done because this simulation can't simulate the true competition of
+         * miners
+         * on a single peice of hardware
+         */
+        Random rand = new Random();
+        int minerIndex = rand.nextInt(this.miners.size());
+        this.minerNum = minerIndex + 1; // stores the index of the 'winning' miner
+        return this.miners.get(minerIndex);
+    }
+
+    /**
+     * Simulates a new transaction on the network.
+     * 
+     * @param transactionData the String representing transaction data being input
+     *                        into the network
+     */
+    public void newTransaction(String transactionData) {
+        Network.transactionNumber++;
+        System.out.println("-----------------------------------------------------------------------------\n"
+                + "TRANSACTION #" + Network.transactionNumber + "\n"
+                + "-----------------------------------------------------------------------------");
+        Miner transactionMiner = this.successfulMiner();
+
+        System.out.println("Sending Transaction data to all miners on the network...\n");
+        Network.delay(2000);
+        if (this.networkChain.getChain().size() == 0) {
+            // this is the case for the genesis block only
+            transactionMiner.newTransaction(transactionData, "0");
+        } else {
+            transactionMiner.newTransaction(transactionData, this.networkChain.getLastBlockHash());
+        }
+
+        Block newlyMinedBlock = transactionMiner.getNewestBlock();
+        System.out.println("Block mined successfuly by Miner " + this.minerNum);
+
+        if (nodeConsensus(newlyMinedBlock)) {
+            // assumes there is at least 1 node in the network
+            this.networkChain = this.nodes.get(0).getProposedBlockChain();
+            System.out.println("Consensus was reached among all network nodes: Network chain updated\n"
+                    + "-----------------------------------------------------------------------------\n\n");
+        } else {
+            System.out.println("Consensus was not reached: Block has been rejected\n"
+                    + "-----------------------------------------------------------------------------\n\n");
+        }
+    }
+
+    // private void displayBlockInfo(Block displayBlock) {
+
+    // }
+
+    private void initNodes(Block newlyMinedBlock) {
+        int nodeNum = 1;
+        System.out.println("\nInitilizing Nodes with new block...");
+        Network.delay(1500);
+        for (Node n : this.nodes) {
+            System.out.print("Node " + nodeNum + " : ");
+            n.updateNode(this.networkChain, newlyMinedBlock);
+            nodeNum++;
+            Network.delay(1500);
+        }
+        System.out.println("\n");
+    }
+
+    private boolean nodeConsensus(Block newlyMinedBlock) {
+        // simulates the consensus of the ArrayList of nodes
+        boolean result = true;
+        this.initNodes(newlyMinedBlock);
+        BlockChain previousChain = this.nodes.get(0).getProposedBlockChain(); // assumes there is at least 1 node in the
+                                                                              // network
+        System.out.println("Starting Node Consensus protocol...");
+
+        for (int i = 0; i < this.nodes.size(); i++) {
+            Network.delay(600);
+            if (!(this.nodes.get(i).isChainValidated())) {
+                result = false; // if any node rejects the block
+                break;
+            } else {
+                System.out.println("Node " + (i + 1) + " proposed chain is valid");
+            }
+
+            if (!(this.nodes.get(i).getProposedBlockChain().equals(previousChain))) {
+                result = false;
+                break;
+            } else {
+                System.out.println("Node " + (i + 1) + " has reached consensus wih previous Nodes");
+            }
+            System.out.print("\n");
+            previousChain = this.nodes.get(i).getProposedBlockChain();
+        }
+        Network.delay(600);
+        return result;
+    }
+
+    protected static void delay(int milliseconds) {
+        // this method wraps around the try/catch seen below to add delay to the
+        // simulator
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ BlockChain_POW_java/Node.java
@@ -0,0 +1,123 @@
+
+import java.util.ArrayList;
+
+/**
+ * This class adds blocks to the block chain.
+ * Multiple instances of this class are needed to confirm a block
+ */
+public class Node {
+
+    private BlockChain currentValidChain;
+    private BlockChain proposedChain;
+    private Block newBlock;
+    private boolean chainValidated = false; // initialized to false when object is created
+
+    /**
+     * Default constructor.
+     */
+    public Node() {
+    }
+
+    /**
+     * Constructor that initializes the Node with the current valid blockchain and
+     * the
+     * newly mined block that needs to be verified.
+     * 
+     * @param currentBlockChain The current valid blockchain, this us the same for
+     *                          every block on the network
+     * @param minedBlock        The newest mined block that needs to be verified by
+     *                          the Nodes on the network
+     */
+    public Node(BlockChain currentBlockChain, Block minedBlock) {
+        this.currentValidChain = currentBlockChain;
+        this.newBlock = minedBlock;
+        this.createProposedChain();
+        this.chainValidated = Node.validateBlockChain(this.proposedChain);
+    }
+
+    /**
+     * Accesses the current valid chain stored in the node.
+     * 
+     * @return the current valid blockchain accepted by the network
+     */
+    public BlockChain getCurrentValidChain() {
+        return this.currentValidChain;
+    }
+
+    /**
+     * Accesses the updated blockchain currently proposed by the node.
+     * 
+     * @return the current blockchain proposed by the node
+     */
+    public BlockChain getProposedBlockChain() {
+        return this.proposedChain;
+    }
+
+    /**
+     * Accesses the current new block contained in the node.
+     * 
+     * @return The new block to be validated by the node
+     */
+    public Block getBlock() {
+        return this.newBlock;
+    }
+
+    /**
+     * Accesses the boolean indicating whether the node has created a valid chain.
+     * 
+     * @return a boolean indicating whether or not the proposed chain is valid
+     */
+    public boolean isChainValidated() {
+        return this.chainValidated;
+    }
+
+    /**
+     * Updates the currently accepted blockchain and the newly mined block so the
+     * Node instance
+     * can be reused.
+     * 
+     * @param currentBlockChain The current valid blockchain, this us the same for
+     *                          every block on the network
+     * @param minedBlock        The newest mined block that needs to be verified by
+     *                          the Nodes on the network
+     */
+    public void updateNode(BlockChain currentBlockChain, Block minedBlock) {
+        // currentValidChain is cloned here to avoid nodes changing each others
+        // BlockChains
+        this.currentValidChain = new BlockChain(new ArrayList<Block>(currentBlockChain.getChain()));
+        this.newBlock = minedBlock;
+        this.createProposedChain();
+        this.chainValidated = Node.validateBlockChain(this.proposedChain);
+    }
+
+    private void createProposedChain() {
+        this.proposedChain = new BlockChain(this.currentValidChain.getChain());
+        this.proposedChain.addBlock(this.newBlock);
+    }
+
+    /**
+     * Method that ensures a BlockChain object is valid by checking and calculating
+     * the hash values of
+     * each block. This ensures the the chain and the blocks within it haven't been
+     * tampered with.
+     * 
+     * @param proposedChain A BlockChain object that has yet to be validated
+     * @return a boolean value indicating validity of the Blockchain argument
+     */
+    public static boolean validateBlockChain(BlockChain proposedChain) {
+        System.out.print("Validating proposed BlockChain...\r");
+        ArrayList<Block> blockList = proposedChain.getChain();
+        String lastHash = "0"; // this will always be the previous hash of the genesis block
+        boolean valid = true; // chain assumed to be valid until proved otherwise
+        for (Block currentBlock : blockList) {
+            String calculatedBlockHash = currentBlock.calculateBlockHash();
+            if (!(lastHash.equals(currentBlock.getPreviousHash())
+                    && currentBlock.getHash().equals(calculatedBlockHash))) {
+                valid = false;
+                break;
+            }
+            lastHash = calculatedBlockHash; // updates lastHash value for next loop iteration
+        }
+        return valid;
+    }
+}
